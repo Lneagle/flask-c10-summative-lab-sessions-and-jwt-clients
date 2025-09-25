2a2dfea304b7cd592bf27007a20188e5b5ba3275
Lacey Murphy <lneagle@gmail.com>
2025-09-25 16:51:05 -0400

    /posts and /login working

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
 
 from config import app, db, api, jwt
-from models import *
+from models import User, Post, UserSchema, PostSchema
 
 # @app.before_request
 # def check_if_logged_in():
@@ -18,6 +18,27 @@ from models import *
 #     if (request.endpoint) not in open_access_list and (not verify_jwt_in_request()):
 #         return {'errors': ['401 Unauthorized']}, 401
 
+class Login(Resource):
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            token = create_access_token(identity=str(user.id))
+            return make_response(jsonify(token=token, user=UserSchema(exclude=('posts',)).dump(user)), 200)
+
+        return {'errors': ['401 Unauthorized']}, 401
+
+class PostIndex(Resource):
+    def get(self):
+        posts = [PostSchema().dump(post) for post in Post.query.all()]
+        return posts, 200
+    
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(PostIndex, '/posts', endpoint='posts')
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(port=5555, debug=True)
